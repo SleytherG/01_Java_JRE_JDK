c5a8075812c2b30c004534d68d0175d8d5d6a327
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2022-12-16 10:26:37 -0500

    Test if

---
+++ src/EjemploCondicionales.java
@@ -0,0 +1,16 @@
+public class EjemploCondicionales {
+ public static void main(String[] args) {
+  int edad = 17;
+  int cantidad = 2;
+  if (edad >= 18) {
+   System.out.println("Usted es mayor de edad");
+   System.out.println("Bienvenido");
+  } else {
+   if ( cantidad >= 2) {
+    System.out.println("Usted es menor de edad pero esta permitido su ingreso");
+   } else {
+    System.out.println("Usted no esta permitido a entrar");
+   }
+  }
+ }
+}

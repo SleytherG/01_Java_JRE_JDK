fb68f8428b6beca4eefe4ed3331af8c938dd6969
Sleyther <sleyther.calsin@cloudcomputing.com.pe>
2022-12-16 11:05:53 -0500

    Ciclos break

--- src/EjercicioTablaMultiplicacion.java
+++ src/EjercicioTablaMultiplicacion.java
@@ -1,9 +1,11 @@
 public class EjercicioTablaMultiplicacion {
  public static void main(String[] args) {
   for(int contador = 0; contador <= 10; contador++) {
+   System.out.println("Tabla del " + contador);
    for (int multiplicacion = 0; multiplicacion <= 10; multiplicacion++) {
     System.out.println(contador + " * " + multiplicacion + " = " + contador * multiplicacion);
    }
+   System.out.println(" ");
   }
  }
 }
